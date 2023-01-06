5902fce74a8f34e561452849bebbe1eaf506928d
starkoka <j22437@inc.kisarazu.ac.jp>
2023-01-06 15:50:37 +0900

    Add fulladder

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from gpiozero import Button
 
+def fulladder(a,b,x): #作成用関数
+    if a+b+x == 0:
+        return 0,0
+    elif a+b+x == 1:
+        return 0,1
+    elif a+b+x == 2:
+        return 1,0
+    else:
+        return 1,1

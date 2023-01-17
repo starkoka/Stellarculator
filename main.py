96e1e8612148142122319cd9afd37a3ef3157b82
starkoka <j22437@inc.kisarazu.ac.jp>
2023-01-17 13:13:04 +0900

    4桁入力で足し算するプログラムをテスト実装

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 from gpiozero import LED
-from gpiozero import Button
+from time import sleep
 
+a = LED(20)
+b = LED(21)
+c = LED(26)
+d = LED(5)
+
+while True:
+        n = input('数字入れてくれ')
+        if n[0] == '1':
+                a.on()
+        else:
+                a.off()
+
+        if n[1] == '1':
+                b.on()
+        else:
+                b.off()
+
+        if n[2] == '1':
+                c.on()
+        else:
+                c.off()
+
+        if n[3] == '1':
+                d.on()
+        else:
+                d.off()

7b3ba50dc561567b2b87e182e282cdcc30a9fb58
starkoka <j22437@inc.kisarazu.ac.jp>
2023-01-19 14:30:00 +0900

    +-対応したかも

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ from gpiozero import LED
 from time import sleep
 
 a = LED(20)
-b = LED(21)
-c = LED(26)
-d = LED(5)
+b = LED(21) #ab = 緑
+c = LED(12)
+d = LED(5) #cd = 青
+s = LED(4)
 
 while True:
         n = input('数字入れてくれ')
         if n[0] == '1':
-                a.on()
+            a.on()
         else:
-                a.off()
+            a.off()
 
         if n[1] == '1':
                 b.on()
         else:
-                b.off()
-
-        if n[2] == '1':
-                c.on()
-        else:
-                c.off()
+            b.off()
 
         if n[3] == '1':
-                d.on()
+            c.on()
         else:
-                d.off()
+            c.off()
+
+        if n[4] == '1':
+            d.on()
+        else:
+            d.off()
+
+        if n[2] == '+':
+            s.off()
+        else:
+            s.on()

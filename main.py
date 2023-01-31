c48fd1ca01fe11f18ba5a84e36433e19fd696546
starkoka <j22437@inc.kisarazu.ac.jp>
2023-01-31 11:35:44 +0900

    あっれれー、おっかしいぞー

--- main.py
+++ main.py
@@ -1,43 +1,72 @@
+from gpiozero import LED
 from gpiozero import Button
 from time import sleep
 
+PI_INPUT_A = LED(20)
+PI_INPUT_B = LED(21)  # ab = 緑
+PI_INPUT_C = LED(15)
+PI_INPUT_D = LED(5)  # cd = 青
+PI_INPUT_S = LED(4)  # +なら0、-なら1
 
-def fulladder(a, b, x):  # 作成用関数
-    if a + b + x == 0:
-        return 0, 0
-    elif a + b + x == 1:
-        return 0, 1
-    elif a + b + x == 2:
-        return 1, 0
+clock = 1
+
+PI_OUTPUT_X = Button(22)
+PI_OUTPUT_Y = Button(26)
+
+
+# PI_OUTPUT_Z = Button(26)
+
+def pi_input(a, b, s, c, d):
+    if a == '1':
+        PI_INPUT_A.on()
     else:
-        return 1, 1
+        PI_INPUT_A.off()
+
+    if b == '1':
+        PI_INPUT_B.on()
+    else:
+        PI_INPUT_B.off()
+
+    if s == '1':
+        PI_INPUT_S.on()
+    else:
+        PI_INPUT_S.off()
+
+    if c == '1':
+        PI_INPUT_C.on()
+    else:
+        PI_INPUT_C.off()
+
+    if d == '1':
+        PI_INPUT_D.on()
+    else:
+        PI_INPUT_D.off()
+
+    return
 
 
-def twoadder(a, b, x, c, d):
-    if x == 1:
-        c = (c - 1) * (-1)
-        d = (d - 1) * (-1)
-    n = fulladder(b, d, x)
-    m = fulladder(a, c, n[0])
+def output():
+    x = 0;
+    y = 0;
+    z = 0
+    x = PI_OUTPUT_X
+    y = PI_OUTPUT_Y
+    z = PI_OUTPUT_Z
 
-    return m[0], m[1], n[1]
-
-
-def allcheck():
-    for x in range(2):
-        if x == 0:
-            X = '+'
-        else:
-            X = '-'
-        for a in range(2):
-            for b in range(2):
-                for c in range(2):
-                    for d in range(2):
-                        print(str(a) + str(b) + X + str(c) + str(d) + ' = ', end="")
-                        r = twoadder(a, b, x, c, d)
-                        print(str(r[0]) + str(r[1]) + str(r[2]))
+    if PI_OUTPUT_X.is_pressed:
+        x = 1
+    if PI_OUTPUT_Y.is_pressed:
+        y = 1
+    if PI_OUTPUT_Z.is_pressed:
+        z = 1
+    return x, y, z
 
 
 while True:
-    q = input("何かを入力したら実行")
-    allcheck()
+    n = input('2ケタ±2ケタの計算式を入れてくれ')
+    if n[2] == '-':
+        S = '1'
+    else:
+        S = '0'
+    pi_input(n[0], n[1], S, n[3], n[4])
+    print("return：" + output())

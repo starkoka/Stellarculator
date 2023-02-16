9b7df6db030822aa974fc8acec9e5ceac0991c91
starkoka <j22437@inc.kisarazu.ac.jp>
2023-02-16 09:17:26 +0900

    内部モードと外部モードを切り替えできるように変更

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from gpiozero import LED
 from gpiozero import Button
 from time import sleep
 
-#PI_INPUT_A = LED(15)
-#PI_INPUT_B = LED(7)  # ab = 緑 ※修正前は逆になる
-#PI_INPUT_C = LED(20)
-#PI_INPUT_D = LED(21)  # cd = 青　※修正前は逆になる
-#PI_INPUT_S = LED(4)  # +なら0、-なら1
-
 clock = 0.05
+CHANGE_EI= 0 #0なら内蔵、1なら外付け
 
-#PI_OUTPUT_X = Button(26)
-#PI_OUTPUT_Y = Button(22)
-#PI_OUTPUT_Z = Button(5)
+if CHANGE_EI == 1:
+    PI_INPUT_A = LED(15)
+    PI_INPUT_B = LED(7)  # ab = 緑 ※修正前は逆になる
+    PI_INPUT_C = LED(20)
+    PI_INPUT_D = LED(21)  # cd = 青　※修正前は逆になる
+    PI_INPUT_S = LED(4)  # +なら0、-なら1
 
-def pi_input(a, b, s, c, d):
+    PI_OUTPUT_X = Button(26)
+    PI_OUTPUT_Y = Button(22)
+    PI_OUTPUT_Z = Button(5)
+
+
+def pi_input(a, b, s, c, d): #ラズパイ入力用関数
     if a == '1':
         PI_INPUT_A.on()
     else:
@@ -43,7 +46,7 @@ def pi_input(a, b, s, c, d):
     return
 
 
-def pi_output():
+def pi_output(): #ラズパイ出力用関数
     x = 0
     y = 0
     z = 0
@@ -57,7 +60,7 @@ def pi_output():
     return x, y, z
 
 
-def fulladder(a, b, x):  # 作成用関数
+def fulladder(a, b, x):  # 内蔵1bit加減算機
     if a + b + x == 0:
         return 0, 0
     elif a + b + x == 1:
@@ -68,7 +71,7 @@ def fulladder(a, b, x):  # 作成用関数
         return 1, 1
 
 
-def twoadder(a, b, x, c, d):
+def twoadder(a, b, x, c, d): #内蔵2bit加減算機
     a = int(a)
     b = int(b)
     x = int(x)
@@ -82,60 +85,71 @@ def twoadder(a, b, x, c, d):
 
     return str(m[0]), str(m[1]), str(n[1])
 
-def allcheck():
-    for x in range(2):
-        if x == 0:
-            X = '+'
-        else:
-            X = '-'
-        for a in range(2):
-            for b in range(2):
-                for c in range(2):
-                    for d in range(2):
-                        print(str(a) + str(b) + X + str(c) + str(d) + ' = ', end="")
-                        r = twoadder(a, b, x, c, d)
-                        pi_input(str(a), str(b), str(x), str(c), str(d))
-                        sleep(clock)
-                        print(str(r[0]) + str(r[1]) + str(r[2]), end="  |  ")
-                        o = pi_output()
-                        print(str(o[0]) + str(o[1]) + str(o[2]))
+def adderfunc(a, b, x, c, d): #切り替え対応関数
+    if CHANGE_EI == 0:
+        return twoadder(a, b, x, c, d)
+    else:
+        pi_input(a, b, x, c, d)
+        return pi_output()
+
+def allcheck(): #外付け回路動作チェック関数
+    if CHANGE_EI == 1:
+        for x in range(2):
+            if x == 0:
+                X = '+'
+            else:
+                X = '-'
+            for a in range(2):
+                for b in range(2):
+                    for c in range(2):
+                        for d in range(2):
+                            print(str(a) + str(b) + X + str(c) + str(d) + ' = ', end="")
+                            r = twoadder(a, b, x, c, d)
+                            pi_input(str(a), str(b), str(x), str(c), str(d))
+                            sleep(clock)
+                            print(str(r[0]) + str(r[1]) + str(r[2]), end="  |  ")
+                            o = pi_output()
+                            print(str(o[0]) + str(o[1]) + str(o[2]))
+    else:
+        print("内蔵モードになっています！")
 
 def add(add1,add2):
     if len(add1) < len(add2):
-        add1,add2 = add2,add1
+        add1, add2 = add2, add1
 
     for i in range(len(add1) - len(add2)):
         add2 = '0' + add2
-    if len(add1) %2 == 1:
+    if len(add1) % 2 == 1:
         add1 = '0' + add1
         add2 = '0' + add2
 
     output = []
     kuriagari = '0'
-    for i in range(len(add1)//2):
+    for i in range(len(add1) // 2):
         if kuriagari == '0':
-            add3 = [add1[len(add1) - (i * 2) - 2],add1[len(add1) - (i * 2) - 1]]
+            add3 = [add1[len(add1) - (i * 2) - 2], add1[len(add1) - (i * 2) - 1]]
             next_kuriagari = '0'
         else:
-            if i%2 == 0 : #片方だけが01を頻繁に入力することを避けるコード
-                adder = twoadder(add1[len(add1) - (i * 2) - 2], add1[len(add1) - (i * 2) - 1], '0', '0', '1')
+            if i % 2 == 0:  # 片方だけが01を頻繁に入力することを避けるコード
+                adder = adderfunc(add1[len(add1) - (i * 2) - 2], add1[len(add1) - (i * 2) - 1], '0', '0', '1')
             else:
-                adder = twoadder('0', '1', '0',add1[len(add1) - (i * 2) - 2], add1[len(add1) - (i * 2) - 1])
-            add3 = [adder[1],adder[2]]
+                adder = adderfunc('0', '1', '0', add1[len(add1) - (i * 2) - 2], add1[len(add1) - (i * 2) - 1])
+            add3 = [adder[1], adder[2]]
             next_kuriagari = adder[0]
-        adder = twoadder(add3[0],add3[1],'0',add2[len(add2) - (i * 2) - 2],add2[len(add2) - (i * 2) - 1])
+        adder = adderfunc(add3[0], add3[1], '0', add2[len(add2) - (i * 2) - 2], add2[len(add2) - (i * 2) - 1])
 
-        output.insert(0,adder[2])
-        output.insert(0,adder[1])
-        kuriagari = max(adder[0],next_kuriagari)
-    output.insert(0,kuriagari)
+        output.insert(0, adder[2])
+        output.insert(0, adder[1])
+        kuriagari = max(adder[0], next_kuriagari)
+    output.insert(0, kuriagari)
     add_result = ""
     for i in range(len(output)):
         add_result = add_result + output[i]
     return add_result
 
+
 def multi(multi1,multi2):
-    multi_result = 0
+    multi_result = '0'
     for i in range(int(multi2,2)):
         multi_result = add(multi_result,multi1)
     return multi_result
@@ -151,4 +165,4 @@ else:
     while True:
         n = str(format(int(input("足す数を入力")), 'b'))
         m = str(format(int(input("足される数を入力")), 'b'))
-        print(int(add(n,m),2))
+        print(int(multi(n,m),2))
